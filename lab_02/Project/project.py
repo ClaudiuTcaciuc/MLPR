71b1deb66ecebb89e6b5d9de4971075d29b41987
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-03-15 12:41:16 +0100

    added project part

--- lab_02/Project/project.py
+++ lab_02/Project/project.py
@@ -1,46 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets
 
-def load_iris_binary():
-    # Load binary iris dataset
-    iris_data = datasets.load_iris()
-    data, label = iris_data['data'].T, iris_data['target']
-    return data, label
-
-def plot_iris_histogram(data, label):
-    # Plot histograms for iris dataset features
-    plt.figure(figsize=(15, 12))
-    classes = ['setosa', 'versicolor', 'virginica']
-    colors = ['blue', 'orange', 'green']
+def load_data():
+    data_matrix = np.loadtxt('trainData.txt', delimiter=',', usecols=range(0, 6), dtype=np.float64)
+    data_labels = np.loadtxt('trainData.txt', delimiter=',', usecols=6, dtype=int)
     
-    for i in range(4):
-        plt.subplot(2, 2, i+1)
-        for j in range(3):
+    return data_matrix.T, data_labels
+
+def plot_histogram(data, label):
+    # Plot histograms for dataset features
+    plt.figure()
+    classes = ["Fake", "Real"]
+    colors = ["blue", "orange"]
+    
+    for i in range(data.shape[0]):
+        plt.subplot(2, 3, i+1)
+        for j in range(2):
             plt.hist(data[i, label == j], alpha=0.5, label=classes[j], density=True, ec=colors[j])
         plt.title(f'Feature {i+1}')
         plt.legend(loc='best')
     plt.tight_layout()
     plt.show()
 
-def plot_iris_scatter(data, label):
-    # Plot scatter plots for iris dataset features
+def plot_scatter(data, label):
+    # Plot scatter plots for dataset features
     plt.figure(figsize=(10, 10))
-    classes = ['setosa', 'versicolor', 'virginica']
-    colors = ['blue', 'orange', 'green']
+    classes = ["Fake", "Real"]
+    colors = ["blue", "orange"]
     
-    for i in range(4):
-        for j in range(4):
+    x, _ = data.shape
+    
+    for i in range(x):
+        for j in range(x):
             if i != j:
-                plt.subplot(4, 4, i*4 + j + 1)
-                for k in range(3):
-                    plt.scatter(data[j, label == k], data[i, label == k], label=classes[k], s=10, c=colors[k])
+                plt.subplot(x, x, i*x + j + 1)
+                for k in range(2):
+                    plt.scatter(data[j, label == k], data[i, label == k], label=classes[k], s=1, c=colors[k])
                 plt.xlabel(f'Feature {j+1}')
                 plt.ylabel(f'Feature {i+1}')
                 plt.legend(loc='best')
             else:
-                plt.subplot(4, 4, i*4 + j + 1)
-                for k in range(3):
+                plt.subplot(x, x, i*x + j + 1)
+                for k in range(2):
                     plt.hist(data[i, label == k], alpha=0.5, label=classes[k], density=True, ec=colors[k])
                 plt.legend(loc='best')
                 plt.ylabel(f'Feature {i+1}')
@@ -70,12 +71,12 @@ def compute_statistics(data):
     print(f'Standard deviation\n{std}')
 
 def main():
-    data, label = load_iris_binary()
+    data, label = load_data()
     # print(f'data shape: {data.shape}')
     # print(f'label shape: {label.shape}')
-    plot_iris_histogram(data, label)
-    plot_iris_scatter(data, label)
+    plot_histogram(data, label)
+    plot_scatter(data, label)
     compute_statistics(data)
-
+    
 if __name__ == "__main__":
     main()
