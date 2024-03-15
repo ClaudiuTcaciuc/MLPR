7200b2ea625455060298c7fc551b7090d0f67952
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2024-03-15 12:20:45 +0100

    Update lab_02.py

--- lab_02/lab_02.py
+++ lab_02/lab_02.py
@@ -1,58 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn import datasets
 
-
-# load binary iris dataset
 def load_iris_binary():
-    data, label = datasets.load_iris()['data'].T, datasets.load_iris()['target']
+    # Load binary iris dataset
+    iris_data = datasets.load_iris()
+    data, label = iris_data['data'].T, iris_data['target']
     return data, label
 
 def plot_iris_histogram(data, label):
+    # Plot histograms for iris dataset features
     plt.figure(figsize=(15, 12))
+    classes = ['setosa', 'versicolor', 'virginica']
+    colors = ['blue', 'orange', 'green']
     
     for i in range(4):
         plt.subplot(2, 2, i+1)
-        plt.hist(data[i, label == 0], alpha=0.5, label='setosa', density=True, ec='blue')
-        plt.hist(data[i, label == 1], alpha=0.5, label='versicolor', density=True, ec='orange')
-        plt.hist(data[i, label == 2], alpha=0.5, label='virginica', density=True, ec='green')
+        for j in range(3):
+            plt.hist(data[i, label == j], alpha=0.5, label=classes[j], density=True, ec=colors[j])
         plt.title(f'Feature {i+1}')
         plt.legend(loc='best')
     plt.tight_layout()
     plt.show()
 
-def plot_scatter(data, label):
+def plot_iris_scatter(data, label):
+    # Plot scatter plots for iris dataset features
     plt.figure(figsize=(10, 10))
-    
-    data_c1 = data[:, label == 0]
-    data_c2 = data[:, label == 1]
-    data_c3 = data[:, label == 2]
+    classes = ['setosa', 'versicolor', 'virginica']
+    colors = ['blue', 'orange', 'green']
     
     for i in range(4):
         for j in range(4):
             if i != j:
                 plt.subplot(4, 4, i*4 + j + 1)
-                plt.scatter(data_c1[i], data_c1[j], label='setosa', s=10)
-                plt.scatter(data_c2[i], data_c2[j], label='versicolor', s=10)
-                plt.scatter(data_c3[i], data_c3[j], label='virginica', s=10)
-                plt.xlabel(f'Feature {i+1}')
-                plt.ylabel(f'Feature {j+1}')
+                for k in range(3):
+                    plt.scatter(data[j, label == k], data[i, label == k], label=classes[k], s=10, c=colors[k])
+                plt.xlabel(f'Feature {j+1}')
+                plt.ylabel(f'Feature {i+1}')
                 plt.legend(loc='best')
-            else: # not needed, but cool
+            else:
                 plt.subplot(4, 4, i*4 + j + 1)
-                plt.hist(data[i, label == 0], alpha=0.5, label='setosa', density=True, ec='blue')
-                plt.hist(data[i, label == 1], alpha=0.5, label='versicolor', density=True, ec='orange')
-                plt.hist(data[i, label == 2], alpha=0.5, label='virginica', density=True, ec='green')
+                for k in range(3):
+                    plt.hist(data[i, label == k], alpha=0.5, label=classes[k], density=True, ec=colors[k])
                 plt.legend(loc='best')
                 plt.ylabel(f'Feature {i+1}')
     plt.tight_layout()
     plt.show()
 
+def compute_statistics(data):
+    # Empirical dataset mean
+    mu_class = np.mean(data, axis=1).reshape(-1, 1)
+    print(f'Empirical dataset mean\n{mu_class}')
+    
+    # Centered data
+    centered_data = data - mu_class
+    print(f'Centered data shape\n{centered_data.shape}')
+    
+    # Covariance matrix
+    cov_matrix = np.cov(centered_data)
+    print(f'Covariance matrix shape\n{cov_matrix.shape}')
+    # C = (centered_data @ centered_data.T) / (centered_data.shape[1] - 1)
+    
+    # Variance
+    var = np.var(data, axis=1).reshape(-1, 1)
+    print(f'Variance\n {var}')
+    
+    # std
+    std = np.std(data, axis=1).reshape(-1, 1)
+    print(f'Standard deviation\n{std}')
+
 def main():
     data, label = load_iris_binary()
     plot_iris_histogram(data, label)
-    plot_scatter(data, label)
+    plot_iris_scatter(data, label)
+    compute_statistics(data)
 
 if __name__ == "__main__":
     main()
